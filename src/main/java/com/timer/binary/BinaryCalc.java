e87d19c978dfdcd6e059d23ddcfa45340a9a45d2
zhubo <670403684@qq.com>
2018-03-18 23:17:31 +0800

    init

---
+++ src/main/java/com/timer/binary/BinaryCalc.java
@@ -0,0 +1,12 @@
+package com.timer.binary;
+
+/**
+ * Created by zhubo on 2018/3/18.
+ */
+public class BinaryCalc {
+
+    public static void main(String[] args) {
+        int i = 2 ^ 2 ^ 2 ;
+        System.out.println(i);
+    }
+}
